4bce47a04b95f3bae0fe0e58a43fb5c273741636
Pablo Cuevas <cuevasgpablo@gmail.com>
2022-05-18 13:30:53 -0400

    fix footer (?)

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,22 +1,24 @@
 <template>
-    <footer>
-      <Button
-        label="All"
-        icon-src="/src/assets/img/list-icon.png"
-        :active="route.name.includes('all')"
-        @click="to('all')"
-      />
-      <Button
-        label="Favorites"
-        icon-src="/src/assets/img/star-icon.png"
-        :active="route.name.includes('favorites')"
-        @click="to('favorites')"
-      />
-    </footer>
+  <footer>
+    <Button
+      label="All"
+      :icon-src="listIcon"
+      :active="route.name.includes('all')"
+      @click="to('all')"
+    />
+    <Button
+      label="Favorites"
+      :icon-src="starIcon"
+      :active="route.name.includes('favorites')"
+      @click="to('favorites')"
+    />
+  </footer>
 </template>
 
 <script setup>
 import { useRoute, useRouter } from 'vue-router'
+import starIcon from '@/assets/img/star-icon.png'
+import listIcon from '@/assets/img/list-icon.png'
 
 const route = useRoute()
 const router = useRouter()
